9604fe5b13808e4a166b2a7dc0ab162337e7667f
David Andrs <andrsd@gmail.com>
2023-11-29 09:33:09 -0700

    all_to_all: Resize the output buffer based on the number of received values

--- include/mpicpp-lite/impl/Communicator.h
+++ include/mpicpp-lite/impl/Communicator.h
@@ -958,17 +958,12 @@ Communicator::all_to_all(const std::vector<std::vector<T>> & in_values,
     out_offsets[0] = 0;
     for (std::size_t i = 0; i < out_count.size() - 1; i++)
         out_offsets[i + 1] = out_offsets[i] + out_count[i];
-    int n_receive_vals = 0;
-    for (std::size_t i = 0; i < out_count.size(); i++)
-        n_receive_vals += out_count[i];
 
     std::vector<T> in_buffer;
     in_buffer.reserve(n_send_vals);
     for (auto & vec : in_values)
         for (auto & v : vec)
             in_buffer.push_back(v);
-
-    out_values.resize(n_receive_vals, 0);
 
     all_to_all(in_buffer, in_count, in_offsets, out_values, out_count, out_offsets);
 }
@@ -986,6 +981,10 @@ Communicator::all_to_all(const std::vector<T> & in_values,
     assert(in_offsets.size() == size());
     assert(out_counts.size() == size());
     assert(out_offsets.size() == size());
+    int n_receive_vals = 0;
+    for (std::size_t i = 0; i < out_counts.size(); i++)
+        n_receive_vals += out_counts[i];
+    out_values.resize(n_receive_vals);
     MPI_CHECK_SELF(MPI_Alltoallv(in_values.data(),
                                  in_counts.data(),
                                  in_offsets.data(),
