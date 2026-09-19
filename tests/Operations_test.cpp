9c2f8a4b5f3aa4e6066ea4ec999c1604e87ef144
David Andrs <andrsd@gmail.com>
2026-09-19 05:50:44 -0600

    Adding tests for operations

---
+++ tests/Operations_test.cpp
@@ -0,0 +1,68 @@
+#include "gmock/gmock.h"
+#include "mpicpp-lite/mpicpp-lite.h"
+
+namespace mpi = mpicpp_lite;
+
+TEST(OperationsTest, sum)
+{
+    double a = 10;
+    double b = 21;
+    auto oper = mpi::op::sum<double>();
+    EXPECT_DOUBLE_EQ(oper(a, b), 31.);
+}
+
+TEST(OperationsTest, prod)
+{
+    double a = 10;
+    double b = 21;
+    auto oper = mpi::op::prod<double>();
+    EXPECT_DOUBLE_EQ(oper(a, b), 210.);
+}
+
+TEST(OperationsTest, max)
+{
+    double a = 10;
+    double b = 21;
+    auto oper = mpi::op::max<double>();
+    EXPECT_DOUBLE_EQ(oper(a, b), 21.);
+}
+
+TEST(OperationsTest, min)
+{
+    double a = 10;
+    double b = 21;
+    auto oper = mpi::op::min<double>();
+    EXPECT_DOUBLE_EQ(oper(a, b), 10.);
+}
+
+TEST(OperationsTest, logical_and)
+{
+    bool a = true;
+    bool b = false;
+    auto oper = mpi::op::logical_and<bool>();
+    EXPECT_FALSE(oper(a, b));
+}
+
+TEST(OperationsTest, logical_or)
+{
+    bool a = true;
+    bool b = false;
+    auto oper = mpi::op::logical_or<bool>();
+    EXPECT_TRUE(oper(a, b));
+}
+
+TEST(OperationsTest, logical_xor)
+{
+    bool a = true;
+    bool b = true;
+    auto oper = mpi::op::logical_xor<bool>();
+    EXPECT_FALSE(oper(a, b));
+}
+
+TEST(OperationsTest, replace)
+{
+    double a = 10;
+    double b = 13;
+    auto oper = mpi::op::replace<double>();
+    EXPECT_DOUBLE_EQ(oper(a, b), 10);
+}
