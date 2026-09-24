28f9f98c3bf16435982f419c9e8c06e8f45f07a9
David Andrs <andrsd@gmail.com>
2026-09-24 07:03:25 -0600

    Adding `Communicator::delete_attr`

--- include/mpicpp-lite/impl/Communicator.h
+++ include/mpicpp-lite/impl/Communicator.h
@@ -700,6 +700,11 @@ public:
         requires std::copyable<T>
     void set_attr(Key key, const T & val);
 
+    /// Deletes an attribute value associated with a key on a communicator
+    ///
+    /// @param key Key of the attribute to delete
+    void delete_attr(Key key);
+
     /// Abort all tasks in the group of this communicator
     ///
     /// @param errcode Error code to return to invoking environment
@@ -1587,6 +1592,12 @@ inline void
 Communicator::set_attr(Key key, const T & val)
 {
     MPI_CHECK_SELF(MPI_Comm_set_attr(this->comm, key.value(), const_cast<T *>(&val)));
+}
+
+inline void
+Communicator::delete_attr(Key key)
+{
+    MPI_CHECK_SELF(MPI_Comm_delete_attr(this->comm, key.value()));
 }
 
 inline void
