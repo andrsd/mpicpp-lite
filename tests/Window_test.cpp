0ead36091018a017d3136bbfa0e587034e91b15f
David Andrs <andrsd@gmail.com>
2026-09-26 07:49:40 -0600

    Adding API for `Window`
    - set_info
    - info
    - attr
    - set_attr
    - delete_attr
    - allocate
    - create_key

--- tests/Window_test.cpp
+++ tests/Window_test.cpp
@@ -26,4 +26,60 @@ TEST(WindowTest, get)
 
     for (int i = 0; i < n; i++)
         EXPECT_EQ(remote_arr[i], (dest * 10) + i);
+
+    win.free();
+}
+
+TEST(WindowTest, allocate)
+{
+    Communicator comm;
+    constexpr int n = 5;
+    auto [win, base] = Window::allocate<int>(n, Info {}, comm);
+    EXPECT_TRUE(win);
+    EXPECT_NE(base, nullptr);
+
+    auto base_attr = win.attr<void *>(win_base);
+    ASSERT_TRUE(base_attr.has_value());
+    EXPECT_EQ(base_attr.value(), base);
+
+    auto size_attr = win.attr<MPI_Aint>(win_size);
+    ASSERT_TRUE(size_attr.has_value());
+    EXPECT_EQ(size_attr.value(), n * sizeof(int));
+
+    win.free();
+}
+
+TEST(WindowTest, info)
+{
+    Communicator comm;
+    auto [win, base] = Window::allocate<int>(5, Info {}, comm);
+
+    Info info;
+    info.set("no_locks", "true");
+    win.set_info(info);
+
+    auto info_back = win.info();
+    EXPECT_TRUE(info_back.is_valid());
+
+    win.free();
+}
+
+TEST(WindowTest, custom_attributes)
+{
+    Communicator comm;
+    auto [win, base] = Window::allocate<int>(5, Info {}, comm);
+
+    auto key = Window::create_key();
+    int val = 9876;
+    win.set_attr(key, val);
+
+    auto val_back = win.attr<int>(key);
+    ASSERT_TRUE(val_back.has_value());
+    EXPECT_EQ(val_back.value(), 9876);
+
+    win.delete_attr(key);
+    auto val_deleted = win.attr<int>(key);
+    ASSERT_FALSE(val_deleted.has_value());
+
+    win.free();
 }
